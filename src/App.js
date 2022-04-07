9a4d0bc66c62d015bad4d98ad88c7c6d72148d57
trashidi98 <tabish.rashidi@gmail.com>
2022-04-07 19:04:02 -0400

    Adding in chat engine stuff

--- src/App.js
+++ src/App.js
@@ -1,30 +1,69 @@
 import logo from './logo.svg';
-import {ChatEngine} from 'react-chat-engine'
+import {ChatEngine, ChatEngineWrapper, ChatSocket, getChat} from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 import './testpages/HomePage.js'
 import HomePage from './testpages/HomePage';
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://api.chatengine.io/chats/',
+  headers: { 
+    'Project-ID': '6e568c29-44d2-4cb3-805c-5320fc680e65', 
+    'User-Name': 'saima', 
+    'User-Secret': '123'
+  }
+};
+
+function getChats() {
+  axios(config)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+
 
 const App = () => {
 
   if (!localStorage.getItem('username')) return <LoginForm />;
+  var id = 109900
+  var accesskey = "ca-4feb7e6a-c707-4c11-85a9-63ce97eae153"
+  var username = "tabish"
 
   return(
+    // <div>
+    // {/* <ChatEngine
+    //   height="100vh"
+    //   projectID="6e568c29-44d2-4cb3-805c-5320fc680e65"
+    //   userName={localStorage.getItem('username')}
+    //   userSecret={localStorage.getItem('password')}
+    //   renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    //   onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    // /> */}
+    // {/* <div>
+    // <HomePage/>
+    // </div> */}
+    
+    // </div>    
     <div>
-    <ChatEngine
-      height="100vh"
-      projectID="6e568c29-44d2-4cb3-805c-5320fc680e65"
-      //userName="saima"
-      //userSecret="123"
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
-    />
-    <div>
-    <HomePage/>
-    </div>
+
+    <ChatEngineWrapper>
+      <ChatSocket
+        projectID="6e568c29-44d2-4cb3-805c-5320fc680e65"
+        chatID={id}
+        chatAccessKey={accesskey}
+        senderUsername={"saima"}
+      />
+
+      <ChatFeed activeChat={id}/>
+    </ChatEngineWrapper>
+
 
     </div>
   )
