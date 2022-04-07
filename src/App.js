348ba26c997319ef3c72126589ce9c4ee6edb902
Saima Munir <munirsaimapie@gmail.com>
2022-04-07 17:13:18 -0400

    Base App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return(
+    <ChatEngine
+      height="100vh"
+      projectID="6e568c29-44d2-4cb3-805c-5320fc680e65"
+      //userName="saima"
+      //userSecret="123"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  )
 }
 
 export default App;
