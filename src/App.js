38305024ad72f7165f5fe31d5ecf9fb9b9302ab8
Saima Munir <munirsaimapie@gmail.com>
2022-04-07 23:01:09 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-import {ChatEngine, ChatEngineWrapper, ChatSocket, getChat} from 'react-chat-engine';
+import {ChatEngine, ChatEngineWrapper, ChatSocket, getChat, Socket} from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 import './testpages/HomePage.js'
 import HomePage from './testpages/HomePage';
+import { Col } from 'react-grid-system';
 var axios = require('axios');
 
 var config = {
@@ -29,44 +30,26 @@ function getChats() {
 
 
 
-const App = () => {
+const App = props => {
 
   if (!localStorage.getItem('username')) return <LoginForm />;
-  var id = 109900
-  var accesskey = "ca-4feb7e6a-c707-4c11-85a9-63ce97eae153"
-  var username = "tabish"
+  const id = 109900
+  const accesskey = "ca-4feb7e6a-c707-4c11-85a9-63ce97eae153"
+  const username = "tabish"
+  
 
-  return(
-    // <div>
-    // {/* <ChatEngine
-    //   height="100vh"
-    //   projectID="6e568c29-44d2-4cb3-805c-5320fc680e65"
-    //   userName={localStorage.getItem('username')}
-    //   userSecret={localStorage.getItem('password')}
-    //   renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-    //   onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
-    // /> */}
-    // {/* <div>
-    // <HomePage/>
-    // </div> */}
-    
-    // </div>    
+  return (
     <div>
-
-    <ChatEngineWrapper>
-      <ChatSocket
+        <ChatEngineWrapper>
+      <Socket
         projectID="6e568c29-44d2-4cb3-805c-5320fc680e65"
-        chatID={id}
-        chatAccessKey={accesskey}
-        senderUsername={"saima"}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
       />
-
       <ChatFeed activeChat={id}/>
     </ChatEngineWrapper>
-
-
     </div>
-  )
+)
 }
 
 export default App;
